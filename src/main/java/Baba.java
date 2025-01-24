4e487384c11fbf6f3dc99993ef3705ec25b496d3
eth4n <ethantwy@gmail.com>
2025-01-24 15:52:15 +0800

    Level 0. Rename, Greet, Exit.

---
+++ src/main/java/Baba.java
@@ -0,0 +1,10 @@
+public class Baba {
+    public static void main(String[] args) {
+        String logo = "Hello! I'm Baba \n"
+                + "What can I do for you? \n"
+                + "__________________\n"
+                + "Bye. Hope to see you again soon!\n"
+                + "__________________\n";
+        System.out.println("Hello from\n" + logo);
+    }
+}
